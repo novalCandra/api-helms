41eefda5852e17e3d95437e82fffe57943d28d91
novalCandra <dinatanovel5@gmail.com>
2026-01-12 21:36:33 +0700

    feat : config

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'mysql.railway.internal'),
+            'host' => env('DB_HOST', 'mysql-production-e186.up.railway.app'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'railway'),
             'username' => env('DB_USERNAME', 'root'),
